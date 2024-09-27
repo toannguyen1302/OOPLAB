aecc057c00dcdd6371d3807d15f1686effcd5630
toannguyen1302 <nktoan.ltver@gmail.com>
2024-09-27 23:05:47 +0700

    .

--- Exercises/ex61.java
+++ Exercises/ex61.java
@@ -1,18 +1,14 @@
 package Exercises;
 import javax.swing.JOptionPane;
 
-// Question: 
-// +) if users choose Cancel: return same as No
-// +) I have customize in the cmt below
-
 public class ex61 {
 
     public static void main(String[] args) {
-        // int option = JOptionPane.showConfirmDialog(null, "Do you want to change to first class ticket?");
+        int option = JOptionPane.showConfirmDialog(null, "Do you want to change to first class ticket?");
 
-        Object[] options = { "I do", "I don't" };
-        int option = JOptionPane.showOptionDialog(null,"Do you want to change to first class ticket?",
-                "Ticket Confirmation Question",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[0]);
+        // Object[] options = { "I do", "I don't" };
+        // int option = JOptionPane.showOptionDialog(null,"Do you want to change to first class ticket?",
+        //         "Ticket Confirmation Question",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[0]);
 
         JOptionPane.showMessageDialog(null,
                 "You've chosen: " + (option == JOptionPane.YES_OPTION ? "Yes" : "No"));
